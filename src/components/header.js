ef2dbfe66952d7da5a8ad7e40a7f351c42b379a2
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-05-21 02:55:01 +0530

    Added: logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./header.scss";
+import logo from "../assets/images/wevote-logo-low.png";
 
 const Header = () => {
   return (
     <div className="header__container">
+      <img src={logo} alt="logo" className="header__logo" />
       <nav className="header__wrapper">
         <div className="header__item">
-          <a href="#"> Home</a>
+          <a href="/#"> Home</a>
         </div>
         <div className="header__item">
-          <a href="#"> Details</a>
+          <a href="/#"> Details</a>
         </div>
         <div className="header__item">
-          <a href="#">Your View</a>
+          <a href="/#">You</a>
         </div>
         <div className="header__item">
-          <a href="#"> About</a>
+          <a href="/#"> About</a>
         </div>
         <div className="header__item">
-          <a href="#"> Contact</a>
+          <a href="/#"> Contact</a>
         </div>
       </nav>
     </div>
