f72e9ec13674011728e962ea02aaa5ff8204358f
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-06-25 15:51:23 +0530

    fix state deatl card

--- src/components/statepagedetail/StateInfoCard.js
+++ src/components/statepagedetail/StateInfoCard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import mpStateLogo from "../../assets/images/mp-state-logo.jpeg";
 import "./StateInfoCard.scss";
+import { Link } from "react-router-dom";
 
 const StateInfoCard = () => {
   return (
@@ -12,14 +13,32 @@ const StateInfoCard = () => {
             src={mpStateLogo}
             className="stateinfocard--image"
             alt="state__mp"
-            height="150px"
-            width="150px"
+            height="200px"
+            width="200px"
           />
+          <p
+            style={{
+              fontSize: "14px",
+              fontWeight: "500",
+              margin: 0,
+              padding: 0,
+              marginTop: "5px",
+              textAlign: "center",
+            }}
+          >
+            State Official Logo
+          </p>
         </div>
         <div className="stateinfocard__container--detail">
           <p>Capital : Bhopal</p>
           <p>Population : 12123123</p>
           <p>Districts : 50</p>
+          <p>
+            State Website :{" "}
+            <Link to="https://mp.gov.in/" className="link">
+              https://mp.gov.in/
+            </Link>
+          </p>
         </div>
       </div>
     </div>
