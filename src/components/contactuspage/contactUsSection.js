ed22b1e867b29f89d58c38550fb07ec4e3338e9f
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-07-09 20:41:45 +0530

    Added : contact us page

--- src/components/contactuspage/contactUsSection.js
+++ src/components/contactuspage/contactUsSection.js
@@ -12,29 +12,50 @@ const ContactUsSection = () => {
           (CAC/IT/148930 ).
         </p>
 
-        <p className="contact-us-section__para-2">29a Oladimeji Alo Street,</p>
+        <div className="contact-us-section__address-section">
+          <p className="contact-us-section__para-2">
+            29a Oladimeji Alo Street,
+          </p>
+          <p className="contact-us-section__para-2">Off Freedom Way,</p>
+          <p className="contact-us-section__para-2">Lekki Phase 1</p>
+          <p className="contact-us-section__para-2">Lagos, Nigeria</p>
+        </div>
 
-        <p className="contact-us-section__para-2">Off Freedom Way,</p>
-        <p className="contact-us-section__para-2">Lekki Phase 1</p>
-        <p className="contact-us-section__para-2">Lagos, Nigeria</p>
-
-        <p className="contact-us-section__para-2">2348136676447</p>
-        <p className="contact-us-section__para-2">hello@wevote.ng</p>
+        <div className="contact-us-section__address-section">
+          <p className="contact-us-section__para-2">2348136676447</p>
+          <p className="contact-us-section__para-2">hello@wevote.ng</p>
+        </div>
 
         <p className="contact-us-section__para-2">www.acecentre.org</p>
       </div>
 
       <div className="contact-us-section__container-2">
         <h2 className="contact-us-section__heading">Write Us</h2>
-        {/* <p className="contact-us-section__para">
-          We envision a highly democratic country where good governance thrives
-          because of actively engaged citizens.
-        </p>
+        <input
+          placeholder="Your Name"
+          className="contact-us-section__inputField"
+        />
+        <input
+          placeholder="Your Email"
+          className="contact-us-section__inputField"
+        />
+        <input
+          placeholder="Phone Number"
+          className="contact-us-section__inputField"
+        />
+        <input
+          placeholder="Subject"
+          className="contact-us-section__inputField"
+        />
 
-        <h2 className="contact-us-section__heading">Our Values</h2>
-        <p className="contact-us-section__para">
-          We are non-profit, non-partisan, independent, non-governmental.
-        </p> */}
+        <textarea
+          id="w3review"
+          name="w3review"
+          rows="6"
+          cols="50"
+          placeholder="Please Enter Your Message"
+          className="contact-us-section__textArea"
+        />
       </div>
     </div>
   );
