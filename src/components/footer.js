5dc98436f20b46da26923aa1d04190e76e742eb4
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-07-11 23:09:51 +0530

    footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,15 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__container">Footer</div>
+      <div className="footer__container">
+        <div className="footer__left-section">
+          <p></p>
+        </div>
+        <div className="footer__right-section"></div>
+        <p className="footer__copyright">
+          © 2021 WeVote is an initiative of ACE Centre (CAC/IT: 148930)
+        </p>
+      </div>
     </div>
   );
 };
