92acb78de22951ea7a46c8952cf011124439d134
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-07-24 23:16:54 +0530

    added : volunteer section css

--- src/components/volunteer/volunteer.js
+++ src/components/volunteer/volunteer.js
@@ -5,13 +5,23 @@ import "./volunteer.scss";
 const Volunteer = () => {
   return (
     <div className="volunteer">
-      <div className="volnteer__form-container">
-        <p className="volnteer__form-container--heading">Volunteer</p>
-        <p className="volnteer__form-container--para">
+      <div className="volunteer__form-container">
+        <p className="volunteer__form-container--heading">Volunteer</p>
+        <p className="volunteer__form-container--para">
           Add your name to join the WeVote team. We’ll send you important
           updates on how you can get involved, and join our push to get every
           eligible Nigerian to vote on Election Day.
         </p>
+        <form className="form__container">
+          <input className="form__text-field" placeholder="Your name" />
+          <span class="border"></span>
+          <div>
+            <input className="form__text-field" placeholder="Your e-mail" />
+            <span class="border"></span>
+            <input className="form__text-field" placeholder="Phone Number" />
+            <span class="border"></span>
+          </div>
+        </form>
       </div>
       <div className="volunteer__text-container">
         <h1 className="volunteer__text-container--heading">Our Values</h1>
--- src/pages/PartnerWithUsPage/PartnerWithUsPage.js
+++ src/pages/PartnerWithUsPage/PartnerWithUsPage.js
@@ -2,9 +2,15 @@ import React from "react";
 
 import "./PartnerWithUsPage.scss";
 import PartnerWithUsIntro from "../../components/partnerwithuspage/partnerWithUsIntro";
+import Volunteer from "../../components/volunteer/volunteer";
 
 const PartnerWithUsPage = () => {
-  return <PartnerWithUsIntro />;
+  return (
+    <>
+      <PartnerWithUsIntro />
+      <Volunteer />
+    </>
+  );
 };
 
 export default PartnerWithUsPage;
