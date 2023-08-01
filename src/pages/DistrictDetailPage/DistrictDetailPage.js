55a79c4c95182ada89cd34053705c7e19f3842c4
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-01 23:06:41 +0530

    added : district card

--- src/pages/DistrictDetailPage/DistrictDetailPage.js
+++ src/pages/DistrictDetailPage/DistrictDetailPage.js
@@ -3,12 +3,22 @@ import React from "react";
 import "./DistrictDetailPage.scss";
 import { useParams } from "react-router-dom";
 
+import jabalpurImage from "../../assets/images/jabalpur.jpeg";
+
 const DistrictDetailPage = () => {
   const { districtName } = useParams();
   return (
     <div className="districtdetailpage">
       <h1>District Detail Page</h1>
-      <h2>{districtName}</h2>
+
+      <div className="districtdetailpage__container">
+        <div className="districtdetailpage__container-image">
+          <img src={jabalpurImage} alt="we-vote" width="330px" />
+        </div>
+        <div className="districtdetailpage__container-text">
+          <h2>{districtName}</h2>
+        </div>
+      </div>
     </div>
   );
 };
