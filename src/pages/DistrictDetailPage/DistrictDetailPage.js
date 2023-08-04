731e8d6b6d9ce01f57fdc8d6fa23eece3b65161e
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-04 23:52:42 +0530

    Added : district detail

--- src/pages/DistrictDetailPage/DistrictDetailPage.js
+++ src/pages/DistrictDetailPage/DistrictDetailPage.js
@@ -9,14 +9,61 @@ const DistrictDetailPage = () => {
   const { districtName } = useParams();
   return (
     <div className="districtdetailpage">
-      <h1 className="districtdetailpage__title">District Detail Page</h1>
+      <h1 className="districtdetailpage__title" style={{ display: "block" }}>
+        District Detail Page
+      </h1>
 
-      <div className="districtdetailpage__container">
-        <div className="districtdetailpage__container-image">
-          <img src={jabalpurImage} alt="we-vote" width="330px" />
+      <div style={{ display: "flex" }}>
+        <div className="districtdetailpage__container">
+          <div className="districtdetailpage__container-image">
+            <img src={jabalpurImage} alt="we-vote" width="330px" />
+          </div>
+          <div className="districtdetailpage__container-text">
+            <h2>{districtName}</h2>
+          </div>
         </div>
-        <div className="districtdetailpage__container-text">
-          <h2>{districtName}</h2>
+
+        <div
+          className="districtdetailpage__container--text"
+          style={{ alignSelf: "center" }}
+        >
+          <h2
+            style={{
+              fontSize: "20px",
+              textAlign: "left",
+              margin: "50px",
+              color: "green",
+              marginTop: "0px",
+            }}
+          >
+            {districtName}
+          </h2>
+          <p
+            className="districtdetailpage__container-text"
+            style={{
+              fontSize: "16px",
+              textAlign: "left",
+              margin: "50px",
+              marginTop: "0px",
+            }}
+          >
+            Madhya Pradesh is a cluster of innumerable districts and varied
+            topographic and climatic characteristics. Housing the magnificent
+            Vindhyachal mountain range, river valleys and plateau Madhya Pradesh
+            is famous for its wonderful art, craft, music and dance. Rich in
+            geographical varieties, the state also has a good number of
+            important mines that has a good influence on the economy of the
+            country. Engaged mostly in agricultural activities, the state is the
+            homeland of many tribal races. Madhya Pradesh lies in the middle of
+            the India, sharing its border with six neighboring states. The
+            northern border of the state has two neighbor states namely the
+            state of Rajasthan and Uttar Pradesh. The western border of the
+            state is shared by a part of Rajasthan and a part of Maharashtra
+            with Gujarat in between. On the southern part of the state of Madhya
+            Pradesh lie the states Maharashtra and Andhra Pradesh. The entire
+            eastern border of the state is bounded by the states of Chattisgarh
+            and Jharkhand.
+          </p>
         </div>
       </div>
     </div>
